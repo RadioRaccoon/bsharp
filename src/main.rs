bcf39c6f527ddd01a88c2bd128d9f847430244de
raccoon <raccoon@ubuntu2004.myguest.virtualbox.org>
2024-03-28 11:57:27 -0400

    - CLI: Init
    -- Initial commit of the CLI, including welcome message & simple parser
    -- Cleaned up Cargo.toml to include only necessary dependencies
    -- and added more description of the project

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,44 @@
-use std::{io, env, fs};
+use clap::Parser;
+use std::{io, env, fs, process::ExitCode};
+use anyhow::{Context, Result};
 
-use shakmaty::{CastlingMode, Chess, Position};
-use shakmaty::fen::Fen;
-
-use pgn_reader::{Visitor, Skip, RawHeader, BufferedReader, SanPlus};
-
-struct LastPosition {
-    pos: Chess,
+#[derive(Debug, Parser)]
+struct Cli {
+    mode: String,
+    path: std::path::PathBuf,
 }
 
-impl LastPosition {
-    fn new() -> LastPosition {
-        LastPosition { pos: Chess::default() }
-    }
-}
+// Credit to Alefith for the bishop design
+const WELCOME_MSG: &'static str = 
+"
+                                      _O
+                                     / //\\
+  _         _                       {     }
+ | |       | |                       \\___/
+ | |__  ___| |__   __ _ _ __ _ __    (___)
+ | '_ \\/ __| '_ \\ / _` | '__| '_ \\    |_|
+ | |_) \\__ \\ | | | (_| | |  | |_) |  /   \\
+ |_.__/|___/_| |_|\\__,_|_|  | .__/  (_____)
+                            | |    (_______)
+                            |_|    /_______\\
+";
 
-impl Visitor for LastPosition {
-    type Result = Chess;
+fn main() -> Result<()> {
+    // Handle user input
+    let _args = Cli::try_parse()?;
 
-    fn header(&mut self, key: &[u8], value: RawHeader<'_>) {
-        // Support games from a non-standard starting position.
-        if key == b"FEN" {
-            let pos = Fen::from_ascii(value.as_bytes()).ok()
-                .and_then(|f| f.into_position(CastlingMode::Standard).ok());
+    main_menu(_args);
 
-            if let Some(pos) = pos {
-                self.pos = pos;
-            }
-        }
-    }
 
-    fn begin_variation(&mut self) -> Skip {
-        Skip(true) // stay in the mainline
-    }
-
-    fn san(&mut self, san_plus: SanPlus) {
-        if let Ok(m) = san_plus.san.to_move(&self.pos) {
-            self.pos.play_unchecked(&m);
-        }
-    }
-
-    fn end_game(&mut self) -> Self::Result {
-        ::std::mem::replace(&mut self.pos, Chess::default())
-    }
-}
-
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    let file_path = &args[1];
-
-    let pgn = b"1. f3 e5 2. g4 Qh4#";
-
-    let mut reader = BufferedReader::new_cursor(&pgn[..]);
-
-    let mut visitor = LastPosition::new();
-    let pos = reader.read_game(&mut visitor)?;
-
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
-
-    assert!(pos.map_or(false, |p| p.is_checkmate()));
     Ok(())
+}
+
+fn main_menu(_input: Cli) {
+    println!("{}", WELCOME_MSG);
+    println!(
+        "
+Welcome to bsharp! Select an option below:
+[1] Load Game
+[2] Sandbox
+[3] Exit");
 }
