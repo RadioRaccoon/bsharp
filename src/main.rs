caded7467ba7d3ee3acc552a9e6668a9491e079d
raccoon <raccoon@ubuntu2004.myguest.virtualbox.org>
2024-03-31 02:15:31 -0400

    - CLI - Welcome & Skeleton
    -- Created a welcome message for the CLI
    
    -- Started implementing the CLI
    --- Following a Command design pattern approach

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
 use clap::Parser;
-use std::{io, env, fs, process::ExitCode};
-use anyhow::{Context, Result};
+use anyhow::Result;
 
-#[derive(Debug, Parser)]
-struct Cli {
-    mode: String,
-    path: std::path::PathBuf,
-}
+pub mod cli;
 
-// Credit to Alefith for the bishop design
-const WELCOME_MSG: &'static str = 
-"
-                                      _O
-                                     / //\\
-  _         _                       {     }
- | |       | |                       \\___/
- | |__  ___| |__   __ _ _ __ _ __    (___)
- | '_ \\/ __| '_ \\ / _` | '__| '_ \\    |_|
- | |_) \\__ \\ | | | (_| | |  | |_) |  /   \\
- |_.__/|___/_| |_|\\__,_|_|  | .__/  (_____)
-                            | |    (_______)
-                            |_|    /_______\\
-";
+use crate::cli::*;
 
 fn main() -> Result<()> {
     // Handle user input
@@ -29,16 +11,6 @@ fn main() -> Result<()> {
 
     main_menu(_args);
 
-
     Ok(())
 }
 
-fn main_menu(_input: Cli) {
-    println!("{}", WELCOME_MSG);
-    println!(
-        "
-Welcome to bsharp! Select an option below:
-[1] Load Game
-[2] Sandbox
-[3] Exit");
-}
